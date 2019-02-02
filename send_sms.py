2afa100789eaf0ef51fc62fdb56f6f6a1635b06d
Nishikesh Kardak <39300717+nishikeshKardak@users.noreply.github.com>
2019-02-02 17:27:26 +0530

    Print better message along with exception.

--- send_sms.py
+++ send_sms.py
@@ -1,29 +1,42 @@
 import requests
+from requests import exceptions
 import json
 import general
-from config import URL, APIKEY, SECRETKEY, PHONENO
+from config import WAY2SMS_URL, SMS_API_KEY, SMS_SECRET_KEY, PHONE_NO
 
-# sending post request
-def send_post_request(reqUrl, apiKey, secretKey, useType, phoneNo, senderId, textMessage):
+
+# send post request
+def sendPostRequest(req_url, api_key, secret_key, use_type, phone_no, sender_id, text_message):
   req_params = {
-  'apikey':apiKey,
-  'secret':secretKey,
-  'usetype':useType,
-  'phone': phoneNo,
-  'message':textMessage,
-  'senderid':senderId
+  'apikey':api_key,
+  'secret':secret_key,
+  'usetype':use_type,
+  'phone': phone_no,
+  'message':text_message,
+  'sender_id':sender_id
   }
-  return requests.post(reqUrl, req_params)
+  return requests.post(req_url, req_params)
 
 
 def send_sms(message):
     try:
-        # getting response and save as response object
-        response = send_post_request(URL, APIKEY, SECRETKEY, 'stage', PHONENO, 'WAYSMS', message )
+        # get response
+        response = sendPostRequest(WAY2SMS_URL, SMS_API_KEY, SMS_SECRET_KEY, 'prod', PHONE_NO, 'WAYSMS', message )
         # print response if you want
-        print (response.text)
+        res =  response.text
+        if 'success' in res:
+            print('\033[92m' + 'Successfully sent the sms' + '\033[0m')
+    except requests.exceptions.HTTPError as eh:
+        print('\033[91m' + 'An HTTP error occurred. %s' %eh)
+        print('\033[0m') 
+    except requests.exceptions.ConnectionError as ec:
+        print('\033[91m' + 'A Connection error occurred %s' %ec)
+        print('\033[0m')
+    except requests.exceptions.Timeout as et:
+        print('\033[91m' + 'It is ConnectTimeout or ReadTimeout %s' %et)
+        print('\033[0m')
     except Exception as e:
-        print(e)
+        print('\033[91m' + "Something went wrong. Failed to send SMS: %s" % e)
+        print('\033[0m')
     finally :
-        print()
-        exit()
+         pass
